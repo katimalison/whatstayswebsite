f7bb76039b1f4368d16ce8634fc135aba0970409
Kati Malison <katimalison@gmail.com>
2021-10-12 04:25:11 -0400

    Continued to redesign

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
-  $('.picture-box').hover(
+  $('.selection-box').hover(
 
     function() {
       // var colors = ['#ff0000', '#B35A42', '#0000ff'];
       // var random_color = colors[Math.floor(Math.random() * colors.length)];
-      $(this).css({
-        "background-color": 'transparent'
-      });
+      // $(this).css({
+      //   "background-color": 'transparent'
+      // });
       $(this).addClass('animated pulse infinite');
       // $(this).find('.box-text-background').hide();
       // $(this).find('.box-text').show(300);
@@ -19,34 +19,14 @@ $(document).ready(function() {
       // var random_color = colors[Math.floor(Math.random() * colors.length)];
       $(this).removeClass('animated pulse infinite');
       if (!$(this).data('clicked')) {
-        $(this).css({
-          "background-color": "rgba(255,244,194, 0.4)"
-        });
+        // $(this).css({
+        //   "background-color": "rgba(255,244,194, 0.4)"
+        // });
         // $(this).find('.box-text').hide();
         // $(this).find('.box-text-background').show(300);
       }
     }
   );
-
-  // $('.picture-box').click(
-  //
-  //    function () {
-  //      var box_id = '#' + $(this).attr('id') + '-box';
-  //
-  //      if ($(this).is(":visible")) {
-  //        $(this).hide(500);
-  //        $(this).data('clicked', false)
-  //      } else {
-  //        if (allClosed()) {
-  //          $(this).show(500);
-  //        } else {
-  //          $(this).delay(500).show(500);
-  //        }
-  //        $(this).data('clicked', true)
-  //      }
-  //
-  //    },
-  // );
 
   $('.whatstaysheader').hover(
     function() {
@@ -76,7 +56,7 @@ function closeContent(content_id) {
   $(content_id).hide(500);
   $(box_id).data('clicked', false)
   $(box_id).css({"background-color": "rgba(255, 244, 194, 0.4)"});
-  $(box_id).parent().css('background-color', 'transparent');
+  // $(box_id).parent().css('background-color', 'transparent');
   $(box_id).removeClass('animated pulse infinite');
 }
 
@@ -124,10 +104,10 @@ function toggleContent(content_id, box_id) {
   } else {
     if (allClosed()) {
       $(content_id).show(500);
-      $(box_id).parent().css('background-color', "rgba(131,141,176,0.7)");
+      // $(box_id).parent().css('background-color', "rgba(131,141,176,0.7)");
     } else {
       $(content_id).delay(500).show(500);
-      $(box_id).parent().css('background-color', "rgba(131,141,176,0.7)");
+      // $(box_id).parent().css('background-color', "rgba(131,141,176,0.7)");
     }
     $(box_id).data('clicked', true)
   }
@@ -170,6 +150,8 @@ function listenWhatStays() {
 }
 
 function videoALight() {
+
+  $('#videoALight-box').animate({opacity: 0}, 0).css("background-image", "url(image.jpeg)").animate({opacity: 1}, 2500);
   var content_id = "#videoALight";
   var box_id = content_id + "-box";
 
